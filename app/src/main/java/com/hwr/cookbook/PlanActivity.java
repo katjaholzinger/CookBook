9d7cdba79dd2f5971a38bb359689cd4cc215d35c
missymond <missymond@googlemail.com>
2018-03-22 17:42:25 +0100

    implement datalistener

--- app/src/main/java/com/hwr/cookbook/PlanActivity.java
+++ app/src/main/java/com/hwr/cookbook/PlanActivity.java
@@ -1,15 +1,31 @@
 package com.hwr.cookbook;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class PlanActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+
+    private User userSnapshot;
+    private Plan[] plansSnapshot;
+    private Recipe[] recipesSnapshot;
+    private Book[] booksSnapshot;
+    private String currentUserId;
+    private static final String TAG = "LoginActivity";
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -35,9 +51,103 @@ public class PlanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan);
+
+        Intent intent = getIntent();
+        currentUserId = intent.getStringExtra(LoginActivity.UID);
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        DBNewListener();
+    }
+
+    private void DBNewListener() {
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+
+        DatabaseReference currentUserRef = database.child("users").child(currentUserId);
+        ValueEventListener userListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                userSnapshot = dataSnapshot.getValue(User.class);
+                // [START_EXCLUDE]
+
+                // [END_EXCLUDE]
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+            }
+        };
+        currentUserRef.addValueEventListener(userListener);
+
+        DatabaseReference plansRef = database.child("plans").child(currentUserId);
+        ValueEventListener planListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                //TODO Change to Plan
+                Plan[] plans = new Plan[] {};
+                plansSnapshot = dataSnapshot.getValue(plans.getClass());
+                // [START_EXCLUDE]
+
+                // [END_EXCLUDE]
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+            }
+        };
+        plansRef.addValueEventListener(planListener);
+
+        DatabaseReference recipesRef = database.child("recipes").child(currentUserId);
+        final Recipe[] recipes = new Recipe[] {};
+
+        ValueEventListener recipesListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+
+                recipesSnapshot = dataSnapshot.getValue(recipes.getClass());
+                // [START_EXCLUDE]
+
+                // [END_EXCLUDE]
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+            }
+        };
+        recipesRef.addValueEventListener(recipesListener);
+
+        DatabaseReference booksRef = database.child("books").child(currentUserId);
+        final Book[] books = new Book[] {};
+
+        ValueEventListener booksListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+
+                booksSnapshot = dataSnapshot.getValue(books.getClass());
+                // [START_EXCLUDE]
+
+                // [END_EXCLUDE]
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+            }
+        };
+        booksRef.addValueEventListener(booksListener);
     }
 
 }
