117c221e16e441d7e30ef66b106feb0305648cb5
missymond <missymond@googlemail.com>
2018-03-29 15:17:54 +0200

    change Database Class

--- app/src/main/java/com/hwr/cookbook/UI/MainActivity.java
+++ app/src/main/java/com/hwr/cookbook/UI/MainActivity.java
@@ -29,6 +29,7 @@ import com.hwr.cookbook.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Thomas on 18.03.2018.
@@ -38,11 +39,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
 
     private User user;
-    private List<Recipe> recipeList;
-    private List<Plan> planList;
-    private List<Book> bookList;
     private String currentUserId;
     private static final String TAG = "LoginActivity";
+    private Database db;
 
 
 
@@ -61,14 +60,16 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
 
         Log.d("Database", "Database erstellen...");
-        Database db = new Database();
-        mTextMessage.setText(db.getUserName(user));
+        db = new Database();
+        db.newListener();
+
         IngredientList ingredients = new IngredientList();
         ingredients.add(new Ingredient("Salz", 5, "Teelöffel" ));
         ingredients.add(new Ingredient("Wasser", 3, "Liter" ));
-        ingredients.add(new Ingredient("Spaghetti", 500, "Gramm" ));
+        ingredients.add(new Ingredient("Farfalle", 500, "Gramm" ));
         ingredients.normalize(4);
-        Recipe recipe = new Recipe("Spaghetti", ingredients, 4, "pasta", "Wasser mit Salz zum kochen bringen. Wenn das Wasser kocht, die Spaghettis dazugeben. Nach 8 Minuten das Wasser abgießen und die Nudeln abschrecken.");
+
+        Recipe recipe = new Recipe("Farfalle", ingredients, 4, "pasta", "Wasser mit Salz zum kochen bringen. Wenn das Wasser kocht, die Farfalle dazugeben. Nach 12 Minuten das Wasser abgießen und die Nudeln abschrecken.");
         db.setNewRecipe(currentUserId, recipe);
 
         // creates TabLayout and Actionbar
@@ -140,84 +141,15 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void logout() {
-        FirebaseAuth.getInstance().signOut();
+        db.logout();
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 
-    private void DBNewListener() {
-        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
 
-        DatabaseReference userRef = database.child("users").child(currentUserId);
-        userRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                user = snapshot.getValue(User.class);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                System.out.println("The read failed: " + databaseError.getMessage());
-            }
-        });
-
-        DatabaseReference plansRef = database.child("plans").child(currentUserId);
-        planList = new ArrayList<>();
-        plansRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                planList.clear();
-                for (DataSnapshot postSnapshot: snapshot.getChildren()) {
-                    Plan plan = postSnapshot.getValue(Plan.class);
-                    planList.add(plan);
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                System.out.println("The read failed: " + databaseError.getMessage());
-            }
-        });
-
-        DatabaseReference recipesRef = database.child("recipes").child(currentUserId);
-        recipeList = new ArrayList<>();
-        recipesRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                recipeList.clear();
-                for (DataSnapshot postSnapshot: snapshot.getChildren()) {
-                    Recipe recipe = postSnapshot.getValue(Recipe.class);
-                    recipeList.add(recipe);
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                System.out.println("The read failed: " + databaseError.getMessage());
-            }
-        });
-
-        final DatabaseReference booksRef = database.child("books").child(currentUserId);
-        bookList = new ArrayList<>();
-        booksRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                bookList.clear();
-                for (DataSnapshot postSnapshot: snapshot.getChildren()) {
-                    Book book = postSnapshot.getValue(Book.class);
-                    bookList.add(book);
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                System.out.println("The read failed: " + databaseError.getMessage());
-            }
-        });
-    }
 
     public List<Book> getBookList(){
-        return bookList;
+        return db.getBookList();
     }
 
     public User getUser(){
