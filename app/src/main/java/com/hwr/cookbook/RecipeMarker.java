4b95826a474382c580cb8e9bd1608927c17dd785
Sidney Steimel <sixd97@gmail.com>
2018-04-19 17:47:36 +0200

    RecipeMarker implements CalendarEvent and added a CalendarAgendaView

--- app/src/main/java/com/hwr/cookbook/RecipeMarker.java
+++ app/src/main/java/com/hwr/cookbook/RecipeMarker.java
@@ -1,26 +1,113 @@
 package com.hwr.cookbook;
 
+import com.github.tibolte.agendacalendarview.models.BaseCalendarEvent;
+import com.github.tibolte.agendacalendarview.models.CalendarEvent;
+import com.github.tibolte.agendacalendarview.models.DayItem;
+import com.github.tibolte.agendacalendarview.models.WeekItem;
+
+import java.util.Calendar;
 import java.util.Date;
 
 /**
  * Created by Sidney on 22.03.2018.
  */
 
-public class RecipeMarker extends IdentifiableElement {
+public class RecipeMarker extends IdentifiableElement implements CalendarEvent{
 
     public Recipe recipe;
     public int persons;
-    public Date date;
+    public Calendar calendar;
 
     public RecipeMarker () {
         super();
     }
 
-    public RecipeMarker (Recipe recipe, int persons, Date date) {
+    public RecipeMarker (Recipe recipe, int persons, Calendar calendar) {
         super();
         this.recipe = recipe;
         this.persons = persons;
-        this.date = date;
+        this.calendar = calendar;
     }
 
+    @Override
+    public long getId() {
+        return 0;
+    }
+
+    @Override
+    public void setId(long mId) {
+
+    }
+
+    @Override
+    public Calendar getStartTime() {
+        return calendar;
+    }
+
+    @Override
+    public void setStartTime(Calendar mStartTime) {
+
+    }
+
+    @Override
+    public Calendar getEndTime() {
+        return calendar;
+    }
+
+    @Override
+    public void setEndTime(Calendar mEndTime) {
+
+    }
+
+    @Override
+    public String getTitle() {
+        return null;
+    }
+
+    @Override
+    public void setTitle(String mTitle) {
+
+    }
+
+    @Override
+    public Calendar getInstanceDay() {
+        return null;
+    }
+
+    @Override
+    public void setInstanceDay(Calendar mInstanceDay) {
+
+    }
+
+    @Override
+    public DayItem getDayReference() {
+        return null;
+    }
+
+    @Override
+    public void setDayReference(DayItem mDayReference) {
+
+    }
+
+    @Override
+    public WeekItem getWeekReference() {
+        return null;
+    }
+
+    @Override
+    public void setWeekReference(WeekItem mWeekReference) {
+
+    }
+
+    @Override
+    public CalendarEvent copy() {
+        String title = recipe.getName();
+        String description = "Description";
+        String location = persons + " persons";
+        int color = R.color.primary_dark;
+        Calendar startTime = calendar;
+        Calendar endTime = calendar;
+        boolean allDay = true;
+        return new BaseCalendarEvent(title, description, location, color,  startTime,  endTime, allDay);
+    }
 }
--- app/src/main/java/com/hwr/cookbook/UI/FragmentPlaner.java
+++ app/src/main/java/com/hwr/cookbook/UI/FragmentPlaner.java
@@ -6,13 +6,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.tibolte.agendacalendarview.AgendaCalendarView;
+import com.github.tibolte.agendacalendarview.CalendarPickerController;
+import com.github.tibolte.agendacalendarview.models.CalendarEvent;
+import com.github.tibolte.agendacalendarview.models.DayItem;
+import com.hwr.cookbook.Book;
 import com.hwr.cookbook.R;
+import com.hwr.cookbook.RecipeMarker;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by Thomas on 18.03.2018.
  */
 
-public class FragmentPlaner extends Fragment{
+public class FragmentPlaner extends Fragment implements CalendarPickerController {
     public FragmentPlaner() {
         // Required empty public constructor
     }
@@ -22,6 +33,84 @@ public class FragmentPlaner extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_planer, container, false);
+
+        // minimum and maximum date of our calendar
+        // 2 month behind, one year ahead, example: March 2015 <-> May 2015 <-> May 2016
+        Calendar minDate = Calendar.getInstance();
+        Calendar maxDate = Calendar.getInstance();
+
+        minDate.add(Calendar.MONTH, -2);
+        minDate.set(Calendar.DAY_OF_MONTH, 1);
+        maxDate.add(Calendar.YEAR, 1);
+
+        List<CalendarEvent> eventList = new ArrayList<>();
+        mockList(eventList);
+
+
+        View view = inflater.inflate(R.layout.fragment_planer, container, false);
+
+        AgendaCalendarView acview = view.findViewById(R.id.agenda_calendar_view);
+
+        acview.init(eventList, minDate, maxDate, Locale.getDefault(), this);
+
+        return view;
+    }
+
+    @Override
+    public void onDaySelected(DayItem dayItem) {
+
+    }
+
+    @Override
+    public void onEventSelected(CalendarEvent event) {
+
+    }
+
+    @Override
+    public void onScrollToDate(Calendar calendar) {
+
+    }
+
+    private void mockList(List<CalendarEvent> eventList) {
+
+        Book book = TestBook.generateTestBook().get(0);
+        Calendar today = Calendar.getInstance();
+        RecipeMarker marker1 = new RecipeMarker ( book.getRecipes()[0], 5, today);
+        eventList.add(marker1);
+
+        today.add(Calendar.DAY_OF_MONTH, 1);
+        RecipeMarker marker2 = new RecipeMarker( book.getRecipes()[0], 5, today);
+        eventList.add(marker2);
+
+
+
+
+/*
+       Calendar startTime1 = Calendar.getInstance();
+        Calendar endTime1 = Calendar.getInstance();
+        endTime1.add(Calendar.MONTH, 1);
+        BaseCalendarEvent event1 = new BaseCalendarEvent("Thibault travels in Iceland", "A wonderful journey!", "Iceland",
+                ContextCompat.getColor(this, R.color.orange_dark), startTime1, endTime1, true);
+        eventList.add(event1);
+
+        Calendar startTime2 = Calendar.getInstance();
+        startTime2.add(Calendar.DAY_OF_YEAR, 1);
+        Calendar endTime2 = Calendar.getInstance();
+        endTime2.add(Calendar.DAY_OF_YEAR, 3);
+        BaseCalendarEvent event2 = new BaseCalendarEvent("Visit to Dalvík", "A beautiful small town", "Dalvík",
+                ContextCompat.getColor(this, R.color.yellow), startTime2, endTime2, true);
+        eventList.add(event2);
+
+        // Example on how to provide your own layout
+        Calendar startTime3 = Calendar.getInstance();
+        Calendar endTime3 = Calendar.getInstance();
+        startTime3.set(Calendar.HOUR_OF_DAY, 14);
+        startTime3.set(Calendar.MINUTE, 0);
+        endTime3.set(Calendar.HOUR_OF_DAY, 15);
+        endTime3.set(Calendar.MINUTE, 0);
+        DrawableCalendarEvent event3 = new DrawableCalendarEvent("Visit of Harpa", "", "Dalvík",
+                ContextCompat.getColor(this, R.color.blue_dark), startTime3, endTime3, false, R.drawable.common_ic_googleplayservices);
+        eventList.add(event3);
+        */
     }
 }
