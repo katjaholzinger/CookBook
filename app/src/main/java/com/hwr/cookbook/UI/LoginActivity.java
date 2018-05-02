5043625e9580eb31d227f6954f3aaabb8b33e7a6
tommysft <thgaertner.sft@t-online.de>
2018-05-02 13:27:49 +0200

    Login--> Register fix

--- app/src/main/java/com/hwr/cookbook/UI/LoginActivity.java
+++ app/src/main/java/com/hwr/cookbook/UI/LoginActivity.java
@@ -102,7 +102,6 @@ public class LoginActivity extends AppCompatActivity implements
             Intent intent = new Intent(this, RegisterActivity.class);
             intent.putExtra(MAIL, new String[]{textEmail.getText().toString(), textPassword.getText().toString()});
             startActivity(intent);
-            finish();
         } else {
             if (i == R.id.user_login) {
                 signIn(textEmail.getText().toString(), textPassword.getText().toString());
